54020d00d67a446bcfe67c2e41c150fced9ab0ad
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-04-26 09:23:16 -0500

    add package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { ApolloServer } = require('apollo-server');
+
 const resolvers = require('./graphql/resolvers');
 const typeDefs = require('./graphql/typeDefs');
+const { obtenerUsuario } = require('./graphql/context');
 const conectarDB = require('./config/db');
 
 // conectar a la base de datos
@@ -10,9 +12,15 @@ conectarDB();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: obtenerUsuario,
 });
 
 // arrancar el servidor
 server.listen().then(({ url }) => {
   console.log(`Servidor listo en la URL ${url}`);
 });
+
+// // arrancar el servidor
+// server.listen().then(({ url }) => {
+//   console.log(`Servidor listo en la URL ${url}`);
+// });
